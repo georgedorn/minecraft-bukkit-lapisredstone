5df93ab954bf81e38a246591b0c19bf20e11d0fa
nodren <brogers@lovecinema.com>
2011-01-23 09:00:04 -0800

    fixed bug that didn't let lapis behave with other plugins

--- src/com/echo28/bukkit/lapisredstone/LapisRedstoneBlockListener.java
+++ src/com/echo28/bukkit/lapisredstone/LapisRedstoneBlockListener.java
@@ -10,21 +10,23 @@ import org.bukkit.inventory.ItemStack;
 public class LapisRedstoneBlockListener extends BlockListener
 {
 	private LapisRedstone plugin;
-	
-	public LapisRedstoneBlockListener(LapisRedstone instance) {
+
+	public LapisRedstoneBlockListener(LapisRedstone instance)
+	{
 		plugin = instance;
 	}
-	
+
 	@Override
 	public void onBlockDamage(BlockDamageEvent event)
 	{
+		if (event.isCancelled()) { return; }
 		if (event.getDamageLevel() == org.bukkit.block.BlockDamageLevel.BROKEN)
 		{
 			if ((event.getBlock().getType() == Material.REDSTONE_ORE) || (event.getBlock().getType() == Material.GLOWING_REDSTONE_ORE))
 			{
 				Location locy = new Location(event.getBlock().getWorld(), event.getBlock().getX(), event.getBlock().getY(), event.getBlock().getZ(), 0, 0);
 				// Material.INK_SACK
-				event.getBlock().getWorld().dropItem(locy, new ItemStack(Material.INK_SACK, plugin.random(), (byte)4));
+				event.getBlock().getWorld().dropItem(locy, new ItemStack(Material.INK_SACK, plugin.random(), (byte) 4));
 			}
 		}
 	}
